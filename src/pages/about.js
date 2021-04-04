9fd695d569dbf3005458c1551c00a4b8365cd5f6
Miesztex <mqzzzy@gmail.com>
2021-04-04 21:19:10 +0200

    set hero and about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/Layout';
 
 const About = () => {
 	return (
 		<Layout>
-			<h1>about page</h1>
+			<main className='page'>
+				<section className='about-page'>
+					<article>
+						<h2>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Reiciendis, nihil.
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+							distinctio cupiditate amet fugiat ut molestias quod, nam quia
+							expedita architecto?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. A,
+							asperiores?
+						</p>
+						<Link to='contact' className='btn'>
+							contact
+						</Link>
+					</article>
+					<StaticImage
+						src='../assets/images/about.jpeg'
+						alt='salt in bowl'
+						className='about-img'
+						placeholder='blurred'
+					/>
+				</section>
+			</main>
 		</Layout>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Layout from '../components/Layout';
-
+import { StaticImage } from 'gatsby-plugin-image';
 const Index = () => {
 	return (
 		<Layout>
-			<h1>home page</h1>
+			<main className='page'>
+				<header className='hero'>
+					<StaticImage
+						src='../assets/images/main.jpeg'
+						alt='eggs'
+						className='hero-img'
+						placeholder='blurred'
+						layout='fullWidth'
+					/>
+					<div className='hero-container'>
+						<div className='hero-text'>
+							<h1>simply recipes</h1>
+							<h4>No fluff, just recipes</h4>
+						</div>
+					</div>
+				</header>
+			</main>
 		</Layout>
 	);
 };
